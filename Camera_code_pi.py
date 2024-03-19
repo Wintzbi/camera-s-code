fb75d51e7673f752f68cb49718d54e29ddf65356
Leynaïck <80476115+Wintzbi@users.noreply.github.com>
2024-03-19 15:53:01 +0100

    Update Camera_code_pi.py

--- Camera_code_pi.py
+++ Camera_code_pi.py
@@ -1,17 +1,25 @@
 import time
-from picamera2 import Picamera2, Preview
+from picamera2 import PiCamera2, Preview
 
-picam = Picamera2()
+# Création de l'instance PiCamera2
+picam = PiCamera2()
 types = True
 
+# Configuration de l'aperçu
 config = picam.create_preview_configuration()
 picam.configure(config)
 
+# Démarrage de l'aperçu
 picam.start_preview(Preview.QTGL)
-while types:
+
+try:
+    while types:
         picam.start()
         time.sleep(5)
-        types = False
+        # Détecter la pression de la touche "Q"
+        if input("Appuyez sur 'Q' pour arrêter : ").lower() == 'q':
+            types = False
         picam.capture_file("test-python.jpg")
-
-picam.close()
+finally:
+    # Fermeture de la caméra
+    picam.close()
