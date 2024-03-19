b74e5bb40c8bc9854a471ca28ee9dccf4873cb08
Leynaïck <80476115+Wintzbi@users.noreply.github.com>
2024-03-19 15:26:44 +0100

    Update Camera_code_pi.py

--- Camera_code_pi.py
+++ Camera_code_pi.py
@@ -4,15 +4,15 @@ import time
 import subprocess
 from datetime import datetime
 
-# Function to capture a screenshot of a specific window and save it
+# Function to display the captured screenshot
 def display_screenshot(image_path):
     try:
-        # Utilisez subprocess pour appeler le visualiseur d'image
+        # Utilize subprocess to call the image viewer
         subprocess.run(["feh", "--fullscreen", image_path])
     except Exception as e:
         print("An error occurred while displaying the screenshot:", e)
 
-def capture_screen(time): 
+def capture_screen(current_time): 
     try:
         # Capture the screen using PiCamera
         with picamera2.PiCamera() as camera:
@@ -26,25 +26,22 @@ def capture_screen(time):
         frame = cv2.flip(frame, 1)
         
         # Get the current time and format it
-        current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-        current_time_right_format = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+        current_time_str = current_time.strftime("%Y-%m-%d_%H-%M-%S")
         
         # Put the current time on the frame
-        cv2.putText(frame, current_time, (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+        cv2.putText(frame, current_time_str, (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
         
         # Define the path to save the screenshot
-        screenshot_path = r'C:\Users\Leynaïck\Desktop\Spé\Projet\Screenshots\\'
-        screenshot_path = screenshot_path + time + ".png"
+        screenshot_path = "/home/pi/Desktop/Screenshots/"
+        screenshot_filename = screenshot_path + current_time_str + ".png"
         
         # Save the frame as a screenshot
-        cv2.imwrite(screenshot_path, frame)
-
-        display_screenshot(screenshot_path)
+        cv2.imwrite(screenshot_filename, frame)
+        
+        # Display the screenshot
+        display_screenshot(screenshot_filename)
     except Exception as e:
         print("An error occurred:", e)
-
-# Get the current time in the right format
-current_time_right_format = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
 
 try:
     # Main loop to capture video frames
@@ -54,7 +51,7 @@ try:
             break
         elif key & 0xFF == ord('m'):
             # If 'm' is pressed, capture the screen
-            capture_screen(current_time_right_format)
+            capture_screen(datetime.now())
         
 finally:
     cv2.destroyAllWindows()
