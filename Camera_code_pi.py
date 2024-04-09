e79aed3bd7ead2afd14a8b05ca03b6a9b891ac2c
Leynaïck <80476115+Wintzbi@users.noreply.github.com>
2024-04-09 15:02:18 +0200

    Update Camera_code_pi.py

--- Camera_code_pi.py
+++ Camera_code_pi.py
@@ -1,35 +1,16 @@
-import time
-from picamera2 import Picamera2, Preview
+from picamera2 import PiCamera2
+from time import sleep
 
-# Création de l'instance PiCamera2
-picam = Picamera2()
-types = True
+# Initialisation de la caméra
+camera = PiCamera2()
 
-# Configuration de l'aperçu
-config = picam.create_preview_configuration()
-picam.configure(config)
+# Prévisualisation de la caméra pendant 5 secondes
+camera.start_preview()
+sleep(5)
 
-# Démarrage de l'aperçu
-picam.start_preview(Preview.QTGL)
+# Capture de la photo et sauvegarde dans un fichier
+camera.capture('screenshot.jpg')
 
-def take_screenshot():
-    try:
-        # Capture d'une image
-        picam.start()
-        time.sleep(1)  # Attente pour stabiliser l'image
-        picam.capture_file("screenshot.jpg")
-    finally:
-        # Assurez-vous d'arrêter la capture même en cas d'erreur
-        picam.stop()  # Arrêt de la capture
-
-try:
-    while types:
-        # Détecter la pression de la touche "Q" ou "m"
-        key = input("Appuyez sur 'Q' pour arrêter, ou 'm' pour prendre un screenshot : ").lower()
-        if key == 'q':
-            types = False
-        elif key == 'm':
-            take_screenshot()
-finally:
-    # Fermeture de la caméra
-    picam.close()
+# Arrêt de la prévisualisation et libération des ressources
+camera.stop_preview()
+camera.close()
